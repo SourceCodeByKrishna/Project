350f06551c710fcb5bc410fa220053d266691d0d
SourceCodeByKrishna <rr121kk121@gmail.com>
2022-11-19 22:03:16 +0530

    v1

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 void Display();
-int Register();
+void Register();
 int Login();
+void Menu();
+int MenuDisplay();
 void main()
 {
 	Display();
@@ -15,14 +17,23 @@ void Display()
 	while(Exit!=0)
 	{
 		Exit = 0;
+		int Log=0;
 		printf("\n Do you want to");
 		printf("\n \t 1. Register");
 		printf("\n \t 2. Login");
 		switch(Option)
 		{
-			case 1: printf("Register");
+			case 1:
+				Register();
 			break;
-			case 2: printf("Login");
+			case 2:
+				Log = Login();
+				if(Log==1)
+					Menu();
+				else
+					printf("\n Please try again...");
+			break;
+			case 0: return;
 			break;
 			default: printf("Invalid");
 			break;
@@ -32,30 +43,64 @@ void Display()
 }
 
 //Register
-int Register()
+void Register()
 {
+	int ID;
+	int PASSWORD;
+	int REENTRY;
 	printf("\n Enter Your ID:");
+	scanf("%d",&ID);
 	printf("\n Enter Your Password");
+	scanf("%d", &PASSWORD);
 	printf("\n Re-enter Your Password");
-	return 0;
+	scanf("%d", &REENTRY);
+	return;
 }
 
 //Login
 int Login()
 {
+	int ID;
+	int PASSWORD;
 	printf("\n Enter Your ID:");
+	scanf("%d",&ID);
 	printf("\n Enter Your Password");
+	scanf("%d", &PASSWORD);
 	return 0;
 }
 
 //Menu
 void Menu()
 {
+	int Option= MenuDisplay();
+	while(Option!=0)
+	{
+		switch(Option)
+		{
+			case 1:
+			break;
+			case 2:
+			break;
+			case 3:
+			break;
+			case 4:
+			break;
+		}
+	}
+	
+}
+
+//Menu Display
+int MenuDisplay()
+{
+	int Option;
 	printf("\n 1. Add Product");
 	printf("\n 2. Add Sale");
 	printf("\n 3. Get the Sale Report");
 	printf("\n 4. Get the Product Report");
 	printf("\n 0. Exit");
+	scanf("%d", &Option);
+	return Option;	
 }
 
 //Product
